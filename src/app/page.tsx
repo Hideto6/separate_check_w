f44ab39a66b866eb396fe98fc087e2d43ecd42bf
Hideto6 <hideto_shimoie.23@icloud.com>
2025-08-08 16:43:25 +0900

    deleteMember関数の作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,12 @@ export default function Home() {
     }
   };
 
+  const deleteMember = (memberToDelete: string) => {
+    setMembers(members.filter((member) => member !== memberToDelete));
+  };
+
   const createGroup = () => {
     if (groupName && members.length > 1) {
-      // ページ遷移
       router.push("/group");
     } else {
       alert("グループ名と2人以上のメンバーを入力してください");
@@ -26,10 +29,18 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-blue-100 to-blue-400 p-6">
-      <h1 className="text-4xl font-bold text-blue-800 text-center mb-2">ワリタビ</h1>
-      <p className="text-center mb-6 text-blue-600">旅行の割り勘を、もっとスマートに。</p>
+      <h1 className="text-4xl font-bold text-blue-800 text-center mb-2">
+        ワリタビ
+      </h1>
+      <p className="text-center mb-6 text-blue-600">
+        旅行の割り勘を、もっとスマートに。
+      </p>
       <div className="w-80 h-48 my-5 flex items-center justify-center">
-        <img src="/image/travel_icon.png" alt="travel icon" className="w-full h-full object-contain" />
+        <img
+          src="/image/travel_icon.png"
+          alt="travel icon"
+          className="w-full h-full object-contain"
+        />
       </div>
       <input
         type="text"
@@ -58,6 +69,12 @@ export default function Home() {
         {members.map((name) => (
           <span key={name} className="bg-blue-100 p-2 rounded-2xl m-1">
             {name}
+            <button
+              onClick={() => deleteMember(name)}
+              className=" p-1  text-red-500 font-bold"
+            >
+              ×
+            </button>
           </span>
         ))}
       </div>
