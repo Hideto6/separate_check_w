21f4c2512b646102c2ef1840edc52cfedb07ac43
Hideto6 <hideto_shimoie.23@icloud.com>
2025-08-15 09:16:35 +0900

    Group画面編集

--- src/app/(main)/group/page.tsx
+++ src/app/(main)/group/page.tsx
@@ -65,8 +65,8 @@ export default function GroupPage() {
           {groupName}
         </h2>
         <div className="w-full max-w-md p-3 mb-4 bg-white/30 backdrop-blur-sm rounded-lg shadow-sm flex items-center justify-center space-x-2">
-          <p className="font-bold text-blue-800 w-50">メンバー:</p>
-          <button className="flex flex-wrap gap-1">
+          <p className="font-bold text-blue-800 w-20 ">メンバー:</p>
+          <button className="flex flex-wrap gap-1  w-50">
             {members.map((member, index) => (
               <span
                 key={index}
